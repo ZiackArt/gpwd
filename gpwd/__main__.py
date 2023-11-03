d375a567dd0f409c74b64a37b5153b99d7453eac
ZiackArt <ziackart@gmail.com>
2023-11-03 20:36:23 +0100

    Nothing

--- gpwd/__main__.py
+++ gpwd/__main__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 """
-Sherlock: Find Usernames Across Social Networks Module
+Gpwd: Find Usernames Across Social Networks Module
 
 This module contains the main logic to search for usernames at social
 networks.
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     python_version = sys.version.split()[0]
 
     if sys.version_info < (3, 6):
-        print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
+        print("Gpwd requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
         sys.exit(1)
 
     import gpwd
